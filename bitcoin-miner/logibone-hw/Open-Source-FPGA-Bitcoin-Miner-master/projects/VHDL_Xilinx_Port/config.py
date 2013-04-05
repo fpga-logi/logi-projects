99bb50d88943b6e0816456950b4b64f740510b69
Jonathan Piat <jpiat@yaya.laas.fr>
2013-04-05 10:21:32 +0200

    trying to move project to the logi-repo. Still a work in progress

---
+++ bitcoin-miner/logibone-hw/Open-Source-FPGA-Bitcoin-Miner-master/projects/VHDL_Xilinx_Port/config.py
@@ -0,0 +1,11 @@
+fpgaport = "/dev/ttyUSB0"
+pools = [ \
+  {
+    "name": "bitlc", \
+    "servers": [ \
+      {"host": "pool.bitlc.net", "port": 80}, \
+    ], \
+    "username": "hwNBwG", \
+    "password": "prYxXy", \
+  }, \
+]
