8d1db00dae3f7b7ccea6d39c5d4179749cfe90f8
Michael Jones <mjones@valentfx.com>
2013-05-30 17:21:54 -0600

    inverted the nes_data for active high logic.

--- logi-edu/driver/nes_ctl/nes_ctl.vhd
+++ logi-edu/driver/nes_ctl/nes_ctl.vhd
@@ -17,6 +17,7 @@
 -- Additional Comments: 
 -- for info in NES protocol:
 --//http://www.mit.edu/~tarvizo/nes-controller.html
+--Note:  - all nes_dat logic is active low.  Inverting nes_dat for active high logic.
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -53,15 +54,11 @@ architecture arch of nes_ctl is
 	--state variable
 	type state_type is (s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18);
 	signal state_reg, state_next: state_type;
-	--signal reset: std_logic;	--inverted reset signal
+	signal nes_datn: std_logic;
 
 begin
 
-	--led <= x"ff";
-	--reset <= not(n_reset);	--invert the reset signal
-	
-
-	--syncronos logic
+	--synchronous logic
 	process(clk)
 	begin
 		if(reset= '1') then
@@ -77,9 +74,9 @@ begin
 
 	count_next <= count_reg +1;		--increment counter, will overflow
 	nes_tick <= '1' when count_reg = 0 else '0';		-- tick on every overflow
+	nes_datn <= not(nes_dat);
 
-
-		--STATE MACHINE - WORKED
+	--STATE MACHINE
 	process(clk)
 	begin
 		if(clk'event and clk = '1') then
@@ -94,56 +91,56 @@ begin
 					when s1 =>
 						state_next <= s2;
 						nes_lat <= '0';
-						nes_a <= nes_dat;
+						nes_a <= nes_datn;
 					when s2 =>
 						state_next <= s3;
 						nes_clk <= '1';
 					when s3 =>
 						state_next <= s4;
 						nes_clk <= '0';
-						nes_b <= nes_dat;
+						nes_b <= nes_datn;
 					when s4 =>
 						state_next <= s5;
 						nes_clk <= '1';
 					when s5 =>
 						state_next <= s6;
 						nes_clk <= '0';
-						nes_sel <= nes_dat;						
+						nes_sel <= nes_datn;						
 					when s6 =>
 						state_next <= s7;
 						nes_clk <= '1';
 					when s7 =>
 						state_next <= s8;
 						nes_clk <= '0';
-						nes_start <= nes_dat;
+						nes_start <= nes_datn;
 					when s8 =>
 						state_next <= s9;
 						nes_clk <= '1';
 					when s9 =>
 						state_next <= s10;
 						nes_clk <= '0';
-						nes_up <= nes_dat;
+						nes_up <= nes_datn;
 					when s10 =>
 						state_next <= s11;
 						nes_clk <= '1';
 					when s11=>
 						state_next <= s12;
 						nes_clk <= '0';
-						nes_down <= nes_dat;
+						nes_down <= nes_datn;
 					when s12 =>
 						state_next <= s13;
 						nes_clk <= '1';
 					when s13 =>
 						state_next <= s14;
 						nes_clk <= '0';
-						nes_left <= nes_dat;
+						nes_left <= nes_datn;
 					when s14 =>
 						state_next <= s15;
 						nes_clk <= '1';
 					when s15 =>
 						state_next <= s16;
 						nes_clk <= '0';
-						nes_right <= nes_dat;
+						nes_right <= nes_datn;
 					when s16 =>
 						state_next <= s17;
 						nes_clk <= '1';
