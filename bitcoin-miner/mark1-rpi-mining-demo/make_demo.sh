3b9df4e53415a7d005a1e1478faab3cf6572f89f
Jonathan Piat <piat.jonathan@gmail.com>
2013-06-05 09:12:24 +0200

    modification on teh python driving code

--- bitcoin-miner/mark1-rpi-mining-demo/make_demo.sh
+++ bitcoin-miner/mark1-rpi-mining-demo/make_demo.sh
@@ -7,8 +7,9 @@ UBUNTU='cat /etc/issue | grep Ubuntu';
 if [ -n "$UBUNTU" ]; then
  apt-get install python-json python-mmap
 else
- opkg install python-json python-mmap
+ pacman -U python-json python-mmap
 fi
+logi_loader ./mark1_rpi_mining.bit
 echo "wait for the following script to end"
 echo "result should be : nonce :7a33330e "
 echo "1) Create an account on mining pool (tested on btcguild and bitlc)"
@@ -17,4 +18,4 @@ echo "3) Fill config.py with your worker configuration"
 echo "4) Launch python mark1_rpi_miner.py and wait to get rich ..."
 echo "INFO: the hashrate (~1.8MHash/s) is intentionnaly limited to prevent the FPGA from overheating."
 echo "Because of the low hashrate (compared to bigger FPGA or ASIC), expect to wait some time before getting a valid share ..."
-
+python mark1.py
