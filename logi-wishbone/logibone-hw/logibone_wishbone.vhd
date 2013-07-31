1bc28bd0edc5fa1ccdb57f0adbe4fac3126f2d75
Jonathan Piat <piat.jonathan@gmail.com>
2013-07-31 16:16:31 +0200

    new project to test wishbone interface

--- logi-wishbone/logibone-hw/logibone_wishbone.vhd
+++ logi-wishbone/logibone-hw/logibone_wishbone.vhd
@@ -29,6 +29,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+library work ;
+use work.logi_wishbone_pack.all ;
+use work.logi_wishbone_peripherals_pack.all ;
+
 entity logibone_wishbone is
 port( OSC_FPGA : in std_logic;
 		PB : in std_logic_vector(1 downto 0);
@@ -45,6 +49,21 @@ end logibone_wishbone;
 
 architecture Behavioral of logibone_wishbone is
 
+	component clock_gen
+	port
+	(-- Clock in ports
+		CLK_IN1           : in     std_logic;
+		-- Clock out ports
+		CLK_OUT1          : out    std_logic;
+		CLK_OUT2          : out    std_logic;
+		-- Status and control signals
+		LOCKED            : out    std_logic
+	);
+	end component;
+
+ -- syscon
+ signal sys_reset, sys_clk, clock_locked : std_logic ;
+ signal clk_100Mhz, clk_50Mhz : std_logic ;
  -- wrapper
  signal intercon_wrapper_wbm_address :  std_logic_vector(15 downto 0);
  signal intercon_wrapper_wbm_readdata :  std_logic_vector(15 downto 0);
@@ -54,7 +73,25 @@ architecture Behavioral of logibone_wishbone is
  signal intercon_wrapper_wbm_ack :  std_logic;
  signal intercon_wrapper_wbm_cycle :  std_logic;
 
+
+ signal loopback_sig : std_logic_vector(15 downto 0);
 begin
+
+sys_reset <= NOT PB(0); 
+
+pll0 : clock_gen
+  port map
+   (-- Clock in ports
+    CLK_IN1 => OSC_FPGA,
+    -- Clock out ports
+    CLK_OUT1 => clk_100Mhz,
+    CLK_OUT2 => clk_50Mhz,
+    -- Status and control signals
+    LOCKED => clock_locked);
+
+sys_clk <= clk_100Mhz ;
+GPMC_CLK <= clk_50Mhz;
+
 
 gpmc2wishbone : gpmc_wishbone_wrapper 
 port map
@@ -80,6 +117,29 @@ port map
     );
 
 
+-- Intercon -----------------------------------------------------------
+-- no intercon, direct connection to register with no address decoding
+-----------------------------------------------------------------------
+
+register0 : wishbone_register
+	 port map
+	 (
+		  -- Syscon signals
+		  gls_reset   => sys_reset ,
+		  gls_clk     => sys_clk ,
+		  -- Wishbone signals
+		  wbs_add      => '0' ,
+		  wbs_writedata => intercon_wrapper_wbm_writedata,
+		  wbs_readdata  => intercon_wrapper_wbm_readdata,
+		  wbs_strobe    => intercon_wrapper_wbm_strobe,
+		  wbs_cycle     => intercon_wrapper_wbm_cycle,
+		  wbs_write     => intercon_wrapper_wbm_write,
+		  wbs_ack       => intercon_wrapper_wbm_ack,
+		  -- out signals
+		  reg_out => loopback_sig,
+		  reg_in => loopback_sig
+	 );
+	LED <= loopback_sig(1 downto 0);
 
 end Behavioral;
 
