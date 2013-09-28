fa57bfe458a82c5b2ed2abb9b9cb98f81cabff58
Jonathan Piat <piat.jonathan@gmail.com>
2013-09-28 23:34:26 +0200

    new files for the AVC project

---
+++ AVC2013/avc_platform/pid_controller.vhd
@@ -0,0 +1,173 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:24:43 05/12/2013 
+-- Design Name: 
+-- Module Name:    pid_controller - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+library work ;
+use work.utils_pack.all ;
+
+
+-- gain are fixed point values 8 bit integer part, 8 bit fractionnal part
+entity pid_controller is
+	generic(CLK_PERIOD_NS : positive := 10; 
+			  CTRL_PERIOD_NS : positive := 1000000; 
+			  CMD_WIDTH : positive := 8;
+			  CMD_OFFSET : positive := 127);
+	port(clk, resetn : in std_logic ;
+	     en, reset : in std_logic ;
+		  speed_input : in signed(15 downto 0);
+		  P, I, D : in signed(15 downto 0);
+		  ENC_A : in std_logic ;
+		  ENC_B : in std_logic ;
+		  encoder_count : out signed(15 downto 0);
+		  command : out std_logic_vector(CMD_WIDTH-1 downto 0)
+	);
+
+end pid_controller;
+
+architecture Behavioral of pid_controller is
+constant clk_tick_per_period : integer := CTRL_PERIOD_NS/CLK_PERIOD_NS;
+constant command_max : integer := (2**command'length)-1 ;
+
+
+signal clk_tick_counter : std_logic_vector(nbit(clk_tick_per_period) downto 0);
+signal speed_input_latched, encoder_value, encoder_value_latched : signed(15 downto 0) ;
+signal error, error_grad, error_sum, last_error : signed(15 downto 0) ;
+signal error_sum_enlarged : signed(31 downto 0) ;
+signal std_encoder_value : std_logic_vector(15 downto 0) ;
+signal command_temp, command_offset : signed(31 downto 0);
+signal P_contrib, I_contrib, D_contrib : signed(31 downto 0);
+signal reset_encoder : std_logic ;
+signal ENC_A_OLD, ENC_A_RE : std_logic ;
+signal error_32, error_sum_32 : signed(31 downto 0);
+begin
+	process(clk, resetn)
+	begin
+		if resetn = '0' then
+			ENC_A_OLD <= '0';
+		elsif clk'event and clk = '1' then
+			ENC_A_OLD <= ENC_A ;
+		end if ;
+	end process ;
+	ENC_A_RE <= (ENC_A and (NOT ENC_A_OLD)) and  en;	
+	
+	encoder_chan0 : up_down_counter
+	 generic map(NBIT => 16)
+	 port map( clk => clk,
+				  resetn => resetn,
+				  sraz => reset_encoder,
+				  en => ENC_A_RE ,  -- must detect rising edge
+				  load => '0',
+				  up_downn => ENC_B,
+				  E => (others => '0'),
+				  Q => std_encoder_value
+				  );
+	 encoder_value <= signed(std_encoder_value) ;
+	 
+	process(clk, resetn)
+	begin
+		if resetn = '0' then
+			clk_tick_counter <= (others => '0');
+		elsif clk'event and clk = '1' then
+			if clk_tick_counter = clk_tick_per_period then
+				clk_tick_counter <= (others => '0');
+			else
+				clk_tick_counter <= clk_tick_counter + 1 ;
+			end if ;
+		end if ;
+	end process ;
+	
+	process(clk, resetn)
+	begin
+		if resetn = '0' then
+			encoder_value_latched <= (others => '0');
+			speed_input_latched <= (others => '0');
+			last_error <= (others => '0');
+		elsif clk'event and clk = '1' then
+			if clk_tick_counter = clk_tick_per_period then
+				encoder_value_latched <= encoder_value ;
+				speed_input_latched <= speed_input ;
+				last_error <= error ;
+			end if ;
+		end if ;
+	end process ;
+	reset_encoder <= '1' when clk_tick_counter = clk_tick_per_period else
+						  '0' ;
+						  
+						  
+	 error <= speed_input_latched - encoder_value_latched ;
+	 -- derivative term
+	 error_grad <= error - last_error ;
+	 
+	 error_32 <= RESIZE(error, 32) ;
+	 error_sum_32 <= RESIZE(error_sum, 32) ;
+	 
+	 error_sum_enlarged <= error_sum_32 + error_32 ;
+	 
+	integral_term_latch:process(clk, resetn)
+	begin
+		if resetn = '0' then
+			error_sum <= (others => '0');
+		elsif clk'event and clk = '1' then
+			if clk_tick_counter = 0 then
+				if error_sum_enlarged > (2**15 - 1) then -- handling saturation
+					error_sum <= X"7FFF" ; 
+				elsif error_sum_enlarged < (-(2**15)) then
+					error_sum <= X"8000" ; 
+				else
+					error_sum <= error_sum + error ; 
+				end if ;
+			end if ;
+		end if ;
+	end process ;
+	
+	contrib_latch :process(clk, resetn)
+	begin
+		if resetn = '0' then
+			P_contrib <= (others => '0');
+			I_contrib <= (others => '0');
+			D_contrib <= (others => '0');
+		elsif clk'event and clk = '1' then
+			P_contrib <= P*error ;
+			I_contrib <= I*error_sum ;
+			D_contrib <= D*error_grad ;
+		end if ;
+	end process ;
+	
+	command_temp <= P_contrib + I_contrib + D_contrib ; -- need adder tree
+	command_offset <= command_temp + to_signed((CMD_OFFSET*(2**16)), 32) ;
+	command <= 	(others => '1') when command_offset(31 downto 16) > command_max else
+					(others => '0') when command_offset(31 downto 16) < 0 else
+					std_logic_vector(command_offset((15+CMD_WIDTH) downto 16));
+	
+
+end Behavioral;
+
---
+++ AVC2013/avc_platform/pid_filter.vhd
@@ -0,0 +1,232 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:37:14 07/01/2013 
+-- Design Name: 
+-- Module Name:    pid_filter - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+library work ;
+use work.utils_pack.all ;
+
+
+
+-- inspired by http://crap.gforge.inria.fr/doc2.php
+entity pid_filter is
+generic(clk_period_ns : integer := 8;
+		  pid_period_ns : integer := 20000000); -- 50Hz PID for RC based ESC
+port(
+	clk, resetn : in std_logic ;
+	en : in std_logic ;
+	K, AK : in std_logic_vector(15 downto 0);
+	B : in std_logic_vector(15 downto 0);
+	setpoint : in signed(15 downto 0);
+	ENC_A : in std_logic ;
+	ENC_B : in std_logic ;
+	cmd : out std_logic_vector(15 downto 0);
+	dir : out std_logic 
+);
+end pid_filter;
+
+architecture Behavioral of pid_filter is
+constant tick_modulo : integer := (pid_period_ns/clk_period_ns)-1 ;
+
+
+signal ENC_A_OLD, ENC_A_RE : std_logic ;
+signal std_encoder_value : std_logic_vector(15 downto 0) ;
+signal encoder_value : signed(15 downto 0) ;
+
+
+signal tick_count : std_logic_vector(nbit(tick_modulo)-1 downto 0);
+signal cycle_count : std_logic_vector(1 downto 0);
+signal en_cycle_counter : std_logic ;
+signal reload : std_logic ;
+signal acc, sum : signed(31 downto 0); 
+signal mult_op : unsigned(31 downto 0);
+signal op1, op2 : unsigned(15 downto 0);
+signal mc, xn, xnn : signed(15 downto 0);
+
+signal xn_sign, xnn_sign, mc_sign, sign, sign_latched : std_logic ;
+signal latch_res : std_logic ;
+begin
+
+-- encoder management part
+process(clk, resetn)
+	begin
+		if resetn = '0' then
+			ENC_A_OLD <= '0';
+		elsif clk'event and clk = '1' then
+			ENC_A_OLD <= ENC_A ;
+		end if ;
+	end process ;
+	ENC_A_RE <= (ENC_A and (NOT ENC_A_OLD)) and  en;	
+
+encoder_chan : up_down_counter
+	 generic map(NBIT => 16)
+	 port map( clk => clk,
+				  resetn => resetn,
+				  sraz => reload,
+				  en => ENC_A_RE ,  -- must detect rising edge
+				  load => '0',
+				  up_downn => ENC_B,
+				  E => (others => '0'),
+				  Q => std_encoder_value
+				  );
+	 encoder_value <= signed(std_encoder_value) ;
+
+
+
+counter0 : up_down_counter
+				generic map(NBIT => nbit(tick_modulo))				
+				port map(
+					clk => clk,
+					resetn => resetn ,
+					sraz => '0' ,
+					en => '1' ,
+					up_downn => '0',
+					load => reload,
+					E => std_logic_vector(to_unsigned(tick_modulo, nbit(tick_modulo))),
+					Q => tick_count
+				);
+reload	<= '1' when tick_count = 0 else
+				'0' ;
+
+
+cycles_counter : up_down_counter
+				generic map(NBIT => 2)				
+				port map(
+					clk => clk,
+					resetn => resetn ,
+					sraz => '0' ,
+					en => en_cycle_counter ,
+					up_downn => '0',
+					load => reload,
+					E => (others => '1'),
+					Q => cycle_count
+				);
+en_cycle_counter <= '1' when cycle_count /= 0 else
+						  '0';
+						
+process(clk, resetn)
+begin
+	if resetn = '0' then
+		mult_op <= (others => '0') ;
+	elsif clk'event and clk = '1' then
+		if reload = '1' then
+			mult_op <= (others => '0') ;
+		else
+			mult_op <= op1 * op2 ;
+		end if ;
+	end if ;
+end process ;
+
+
+sum <= (acc - signed(mult_op)) when sign_latched = '0' else
+		 (acc + signed(mult_op)) ;
+		 
+process(clk, resetn)
+begin
+	if resetn = '0' then
+		acc <= (others => '0') ;
+		sign_latched <= '0' ;
+	elsif clk'event and clk = '1' then
+		if reload = '1' then
+			acc <= (others => '0') ;
+		else
+			acc <=  sum;
+		end if ;
+		sign_latched <= sign ;
+	end if ;
+end process ;
+
+with cycle_count select 
+op1 <= unsigned(K) when "11",
+		 unsigned(AK) when "10",
+		 unsigned(B) when "01",
+		 (others => '0') when others;
+		 
+with cycle_count select 
+op2 <= unsigned(abs(xn)) when "11",
+		 unsigned(abs(xnn)) when "10",
+		 unsigned(abs(mc)) when "01",
+		 (others => '0') when others;
+
+with cycle_count select 
+sign <= xn_sign when "11",
+		  (NOT xnn_sign) when "10",
+		  mc_sign when "01",
+		  '0' when others;
+
+xn_sign <= '1' when xn > 0 else
+			  '0' ;
+xnn_sign <= '1' when xnn > 0 else
+			  '0' ;
+mc_sign <= '1' when mc > 0 else
+			  '0' ;
+
+process(clk, resetn)
+begin
+	if resetn = '0' then
+		xnn <= (others => '0') ;
+		xn <= (others => '0') ;
+	elsif clk'event and clk = '1' then
+		if reload = '1' then
+			xn <= setpoint - encoder_value ;
+			xnn <= xn ;
+		end if ;
+	end if ;
+end process ;
+
+
+delay_latch_res : generic_delay
+	generic map( WIDTH => 1, DELAY => 4)
+	port map(
+		clk => clk, resetn => resetn,
+		input(0)	=> reload,
+		output(0) => latch_res
+);		
+
+process(clk, resetn)
+begin
+	if resetn = '0' then
+		mc <= (others => '0') ;
+	elsif clk'event and clk = '1' then
+		if latch_res = '1' then
+			mc <= acc(25 downto 10) ;
+		end if ;
+	end if ;
+end process ;
+
+cmd <=  (others => '0') when en = '0' else
+		  (not  std_logic_vector(mc)) when mc < 0 else
+		  std_logic_vector(mc) ;
+		  
+dir <= '1' when mc < 0 else
+		 '0' ;
+		 
+end Behavioral;
+
