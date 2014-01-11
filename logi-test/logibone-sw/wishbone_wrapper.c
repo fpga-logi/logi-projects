728fd7ea00ef5090d21d4c795b615c0940b0a3cc
Jonathan Piat <piat.jonathan@gmail.com>
2014-01-11 19:41:18 +0100

    new test project for the logibone

---
+++ logi-test/logibone-sw/wishbone_wrapper.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+//#include <linux/ioctl.h>
+#include <sys/ioctl.h>
+
+int fd ;
+
+void logibone_init(){
+	fd = open("/dev/logibone_mem", O_RDWR | O_SYNC);
+}
+
+unsigned int wishbone_write(unsigned char * buffer, unsigned int length, unsigned int address){
+	unsigned int tr_size = 0;
+	unsigned int count = 0 ;
+	if(fd == 0){
+		logibone_init();
+	}
+	count = pwrite(fd, buffer, length, address);
+	return count ;
+}
+unsigned int wishbone_read(unsigned char * buffer, unsigned int length, unsigned int address){
+	unsigned int tr_size = 0;
+	unsigned int count = 0 ;
+	if(fd == 0){
+		logibone_init();
+	}
+	count = pwrite(fd, buffer, length, address);
+	return count ;
+}
+
+
