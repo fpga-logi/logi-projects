df3c083e54ee04871952a35c0ddcf2afabbf853b
Jonathan Piat <piat.jonathan@gmail.com>
2014-03-27 11:43:27 +0100

    software for EDU test

---
+++ logi-edu-test/sw/config.h
@@ -0,0 +1,18 @@
+//DEFINE WHICH TESTS TO RUN
+//COMMENT OUT TESTS YOU DO NOT WANT TO RUN
+#define TEST_SSEG 		//test the SSEG
+
+
+
+
+#ifdef LOGIPI
+
+#define LOAD_CMD "/usr/bin/logi_loader logi_edu_test.bit"
+#define SSEG_0 0x0008
+
+#define LOG_PATH "/home/pi/tests_log/%ld_test.log"
+
+#endif
+
+
+
