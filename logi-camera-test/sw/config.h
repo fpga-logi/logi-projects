81b88e330f40d0d19bb5736ff66164344f14633e
Jonathan Piat <piat.jonathan@gmail.com>
2014-03-29 00:46:47 +0100

    new software for test project

---
+++ logi-camera-test/sw/config.h
@@ -0,0 +1,18 @@
+//DEFINE WHICH TESTS TO RUN
+//COMMENT OUT TESTS YOU DO NOT WANT TO RUN
+
+#ifdef LOGIPI
+
+#define LOAD_CMD "/usr/bin/logi_loader logi_cam_test.bit"
+
+#define FIFO_ADDR 0x0000
+#define FIFO_CMD_OFFSET 0x0004
+#define FIFO_SIZE_OFFSET 0x0004
+#define FIFO_AVAILABLE_OFFSET 0x00041
+
+#define LOG_PATH "/home/pi/tests_log/%ld_test.log"
+
+#endif
+
+
+
