3550b204188178fcc730b3c175f03e83a89260f0
Jonathan Piat <piat.jonathan@gmail.com>
2014-03-31 22:56:33 +0200

    changing repository structure

--- logi-test/logipi-sw/wishbone_wrapper.h
+++
@@ -1,6 +0,0 @@
-
-
-
-
-unsigned int wishbone_write(unsigned char * buffer, unsigned int length, unsigned int address);
-unsigned int wishbone_read(unsigned char * buffer, unsigned int length, unsigned int address);
