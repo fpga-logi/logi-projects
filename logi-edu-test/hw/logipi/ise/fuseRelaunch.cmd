0803a8047acf9c4fe08e7f0855bdf5439af4d19e
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-01 15:35:30 +0200

    modifying repository structure

---
+++ logi-edu-test/hw/logipi/ise/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "/home/jpiat/development/FPGA/logi-family/logi-projects/logi-edu-test/logipi-hw/sseg_tb_isim_beh.exe" -prj "/home/jpiat/development/FPGA/logi-family/logi-projects/logi-edu-test/logipi-hw/sseg_tb_beh.prj" "work.sseg_tb" 
