51eb1fbfbec216b2c0dbe2e4506a502fb3c9087a
Michael Jones <mjones@group8tech.com>
2014-04-07 14:18:12 -0600

    updating the nes edu ucf file.

--- logi-edu/logi-pi-edu/driver/nes_ctl/nes_test.vhd
+++ logi-edu/logi-pi-edu/driver/nes_ctl/nes_test.vhd
@@ -32,54 +32,54 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity nes_test is
 	port(
 			clk : in std_logic;
-			n_reset : in std_logic;
-			--nes1_dat : in std_logic;
+			reset_n : in std_logic;
+			nes1_dat : in std_logic;
 			nes2_dat : in std_logic;
 			nes_lat : out std_logic;
 			nes_clk : out std_logic;	
-			led: out std_logic_vector(7 downto 0)
+			led: out std_logic_vector(1 downto 0)
 			);
 end nes_test;
 
 architecture arch of nes_test is
-	signal nes_a, nes_b, nes_sel, nes_start, nes_up, nes_down, nes_left, nes_right: std_logic;
 	signal nes_clk_buf, nes_lat_buf:std_logic;
+	signal nes1_data_out, nes2_data_out: std_logic_vector(7 downto 0);
 	signal reset: std_logic;
+
 begin
-	reset <= not(n_reset);
+	reset <= not(reset_n);
 
 	nes_lat <= nes_lat_buf;
 	nes_clk <= nes_clk_buf;
 
+	led(0) <= nes1_data_out(0) or nes1_data_out(1) or nes1_data_out(2) or nes1_data_out(3) 
+		or nes1_data_out(4) or nes1_data_out(5) or nes1_data_out(6) or nes1_data_out(7) or 
+		nes2_data_out(0) or nes2_data_out(1) or nes2_data_out(2) or nes2_data_out(3) 
+		or nes2_data_out(4) or nes2_data_out(5) or nes2_data_out(6) or nes2_data_out(7);
 
-	led(0) <= nes_clk_buf;
-	led(1) <= nes_a;
-	led(2) <= nes_b;
-	led(3) <= nes_start;
-	led(4) <= nes_sel;
-	led(5) <= nes_up;
-	led(6) <= nes_down;
-	led(7) <= nes_lat_buf;
-
-
+	led(1) <= nes_lat_buf;
 	
-	nes_unit: entity work.nes_ctl(arch)
+	nes1_unit: entity work.nes_ctl(arch)
 		generic map( N=>17)--17 bit overflow 131k
 		port map(
 				clk=>clk,
 				reset=>reset,
 				--nes_dat=>nes1_dat,
-				nes_dat=>nes2_dat,
+				nes_dat=>nes1_dat,
 				nes_lat=>nes_lat_buf,
 				nes_clk=>nes_clk_buf,	
-				nes_a=>nes_a, 
-				nes_b=>nes_b, 
-				nes_sel=>nes_sel, 
-				nes_start=>nes_start,
-				nes_up=>nes_up, 
-				nes_down=>nes_down, 
-				nes_left=>nes_left, 
-				nes_right=>nes_right			
+				nes_data_out=>nes1_data_out		
+		);
+		
+		nes2_unit: entity work.nes_ctl(arch)
+		generic map( N=>17)--17 bit overflow 131k
+		port map(
+				clk=>clk,
+				reset=>reset,
+				nes_dat=>nes2_dat,
+				nes_lat=>open,
+				nes_clk=>open,	
+				nes_data_out=>nes2_data_out		
 		);
 
 
