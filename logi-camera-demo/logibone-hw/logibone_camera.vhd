20f269f2a983ec2bfd250556c9f700d15a982add
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-11 18:24:23 +0200

    up to date demo for the logibone R1

--- logi-camera-demo/logibone-hw/logibone_camera.vhd
+++ logi-camera-demo/logibone-hw/logibone_camera.vhd
@@ -20,6 +20,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,7 +33,10 @@ use work.logi_wishbone_peripherals_pack.all ;
 use work.interface_pack.all ;
 use work.conf_pack.all ;
 use work.filter_pack.all ;
+use work.feature_pack.all ;
 use work.image_pack.all ;
+use work.classifier_pack.all ;
+
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
 library UNISIM;
@@ -40,7 +44,7 @@ use UNISIM.VComponents.all;
 
 entity logibone_camera is
 port( OSC_FPGA : in std_logic;
-		PB : in std_logic_vector(1 downto 0);
+		PB, SW : in std_logic_vector(1 downto 0);
 		LED : out std_logic_vector(1 downto 0);
 		
 		--PMOD
@@ -65,21 +69,13 @@ architecture Behavioral of logibone_camera is
 		CLK_IN1 : IN std_logic;          
 		CLK_OUT1 : OUT std_logic;
 		CLK_OUT2 : OUT std_logic;
-		CLK_OUT3 : OUT std_logic;
 		LOCKED : OUT std_logic
 		);
 	END COMPONENT;
 
 	
 	signal gls_clk, clk_120, clk_24, clk_locked : std_logic ;
-	signal gls_reset , sys_resetn : std_logic ;
-	
-	signal counter_output : std_logic_vector(31 downto 0);
-	signal fifo_output : std_logic_vector(15 downto 0);
-	signal fifo_input : std_logic_vector(15 downto 0);
-	signal latch_output : std_logic_vector(15 downto 0);
-	signal fifoB_wr, fifoA_rd, fifoA_rd_old, fifoA_empty, fifoA_full, fifoB_empty, fifoB_full : std_logic ;
-	
+	signal gls_reset , gls_resetn : std_logic ;
 	
 	-- wishbone intercon signals
 	signal intercon_wrapper_wbm_address :  std_logic_vector(15 downto 0);
@@ -110,9 +106,34 @@ architecture Behavioral of logibone_camera is
 	signal pixel_y_from_interface, pixel_u_from_interface, pixel_v_from_interface: std_logic_vector(7 downto 0);
 	signal pxclk_from_interface, href_from_interface, vsync_from_interface : std_logic ;
 
-	signal pipeline_reset : std_logic ;
+	signal pixel_from_harris : std_logic_vector(7 downto 0);
+	signal harris_resp : std_logic_vector(15 downto 0);
+	signal signed_harris_resp : signed(15 downto 0);
+	signal pxclk_from_harris, href_from_harris, vsync_from_harris : std_logic ;
+	signal pixel_from_gauss: std_logic_vector(7 downto 0);
+	signal pxclk_from_gauss, href_from_gauss, vsync_from_gauss : std_logic ;
+	signal pixel_from_sobel: std_logic_vector(7 downto 0);
+	signal pxclk_from_sobel, href_from_sobel, vsync_from_sobel : std_logic ;
+	signal pixel_from_switch: std_logic_vector(7 downto 0);
+	signal pxclk_from_switch, href_from_switch, vsync_from_switch : std_logic ;
+	signal pixel_from_classifier: std_logic_vector(7 downto 0);
+	signal pxclk_from_classifier, href_from_classifier, vsync_from_classifier : std_logic ;
 	
-	for all : yuv_register_rom use entity work.yuv_register_rom(ov7725_qvga);
+	signal chist_reset, chist_available : std_logic ;
+	signal chist_pixel_val : std_logic_vector(7 downto 0);
+	signal chist_val_amount : std_logic_vector(31 downto 0);
+	
+	signal switch_value : std_logic_vector(1 downto 0);
+
+	signal preview_fifo_wr : std_logic ;
+	signal preview_fifo_input : std_logic_vector(15 downto 0);
+	signal reset_pipeline : std_logic ;
+	signal cam_conf_reset : std_logic ;	
+	for all : yuv_register_rom use entity work.yuv_register_rom(ov7670_qvga);
+	for all : gauss3x3 use entity work.gauss3x3(RTL);
+	for all : sobel3x3 use entity work.sobel3x3(RTL);
+	constant IMAGE_WIDTH : integer := 320 ;
+	constant IMAGE_HEIGHT : integer := 240 ;
 	
 begin
 	
@@ -126,25 +147,17 @@ begin
 		CLK_IN1 => OSC_FPGA,
 		CLK_OUT1 => gls_clk,
 		CLK_OUT2 => clk_24,
-		CLK_OUT3 => clk_120, --120Mhz system clock
 		LOCKED => clk_locked
 	);
 
+	gls_reset <= not clk_locked ;
+	gls_resetn <= not gls_reset ;
 
-rst_gen : reset_generator -- camera needs a 1ms reset before taking commands
-generic map(HOLD_0	=> 5000)
-port map(clk => gls_clk, 
-	  resetn => PB(0),
-     resetn_0 => sys_resetn
-	  );
-
-gls_reset <= NOT sys_resetn; 
-
-LED(1) <= cam_vsync ;
-LED(0) <= gls_reset ;
+	LED(1) <= cam_vsync ;
+	LED(0) <= gls_reset ;
 
 gpmc2wishbone : gpmc_wishbone_wrapper 
-generic map(sync => true, burst => true)
+generic map(sync => true, burst => false)
 port map
     (
       -- GPMC SIGNALS
@@ -218,16 +231,17 @@ bi_fifo0 : wishbone_fifo
 			wbs_ack      => intercon_fifo0_wbm_ack,
 			
 			
-			wrB => fifoB_wr,
-			rdA => fifoA_rd,
-			inputB => fifo_input, 
-			outputA => fifo_output,
-			emptyA => open,
-			fullA => open,
-			emptyB => open,
-			fullB => open,
-			burst_available_B => open,
-			fifoB_reset => pipeline_reset
+		-- logic signals  
+		wrB => preview_fifo_wr, rdA => '0',
+		inputB => preview_fifo_input,
+		outputA => open,
+		emptyA => open, 
+		fullA => open,
+		emptyB => open, 
+		fullB => open,
+		burst_available_B => open,
+		burst_available_A	=> open,
+		fifoB_reset => reset_pipeline
 		);
  
  conf_rom : yuv_register_rom
@@ -240,8 +254,8 @@ bi_fifo0 : wishbone_fifo
  camera_conf_block : i2c_conf 
 	generic map(ADD_WIDTH => 8 , SLAVE_ADD => "0100001")
 	port map(
-		clock => gls_clk, 
-		resetn => sys_resetn ,		
+		clock => clk_24, 
+		resetn => gls_resetn ,		
  		i2c_clk => clk_24 ,
 		scl => PMOD2(6),
  		sda => PMOD2(2), 
@@ -251,7 +265,7 @@ bi_fifo0 : wishbone_fifo
 		
  camera0: yuv_camera_interface
 		port map(clock => gls_clk,
-					resetn => sys_resetn,
+					resetn => gls_resetn,
 					pixel_data => cam_data, 
 					pxclk => cam_pclk, href => cam_href, vsync => cam_vsync,
 					pixel_clock_out => pxclk_from_interface, hsync_out => href_from_interface, vsync_out => vsync_from_interface,
@@ -267,55 +281,153 @@ bi_fifo0 : wishbone_fifo
 	cam_href <= PMOD2(1) ;
 	cam_vsync <= PMOD2(5) ;
 	PMOD2(0) <= cam_reset ;
-	cam_reset <= PB(0) ;
-
+	cam_reset <= gls_resetn ;
 	
---yuv_pix2fifo : yuv_pixel2fifo 
---port map(
---	clk => gls_clk, resetn => sys_resetn,
---	sreset => pipeline_reset ,
---	pixel_clock => pxclk_from_interface, hsync => href_from_interface, vsync => vsync_from_interface,
---	pixel_y => pixel_y_from_interface,
---	pixel_u => pixel_u_from_interface,
---	pixel_v => pixel_v_from_interface,
---	fifo_data => fifo_input, 
---	fifo_wr => fifoB_wr 
---);
-
-
-adapt_bin : adaptive_pixel_class
-		generic map(nb_class => 2)
-		port map(
-			clk => clk,
-			resetn => resetn,
-			pixel_clock => pxclk_from_interface,
-			hsync => hsync,
-			vsync => vsync,
-			pixel_data_in => pixel_data_in,
-			pixel_clock_out => class_pxclk_out, 
-			hsync_out => class_hsync_out, 
-			vsync_out => class_vsync_out,
-			pixel_data_out => pixel_class_out,
+	
+	chist : cumulative_histogram
+	generic map(image_height => IMAGE_HEIGHT)
+	port map(
+		clk => gls_clk,
+		resetn => gls_resetn,
+		pixel_clock => pxclk_from_interface, 
+		hsync => href_from_interface, 
+		vsync => vsync_from_interface, 
+		pixel_data_in => pixel_y_from_interface,
+		reset_chist => chist_reset,
+		chist_available => chist_available,
+		chist_pixel_val => chist_pixel_val,
+		chist_val_amount => chist_val_amount
+	);
+	
+	pixel_class : adaptive_pixel_class
+			generic map(image_width => IMAGE_WIDTH,
+						image_height => IMAGE_HEIGHT,
+						nb_class => 64)
+			port map(
+			clk => gls_clk, 
+			resetn => gls_resetn,
+			pixel_clock => pxclk_from_interface, 
+			hsync => href_from_interface, 
+			vsync => vsync_from_interface, 
+			pixel_data_in => pixel_y_from_interface,
+			pixel_clock_out => pxclk_from_classifier, 
+			hsync_out => href_from_classifier, 
+			vsync_out => vsync_from_classifier,
+			pixel_data_out => pixel_from_classifier,
 			chist_addr => chist_pixel_val,
 			chist_data => chist_val_amount,
-			chist_available => chist_available ,
-			chist_reset => reset_chist_run 
+			chist_available => chist_available,
+			chist_reset => chist_reset
+			);
+
+	
+	gauss3x3_0	: gauss3x3 
+		generic map(WIDTH => IMAGE_WIDTH,
+				  HEIGHT => IMAGE_HEIGHT)
+		port map(
+					clk => gls_clk ,
+					resetn => gls_resetn ,
+					pixel_clock => pxclk_from_interface, 
+					hsync => href_from_interface, 
+					vsync =>  vsync_from_interface,
+					pixel_clock_out => pxclk_from_gauss, 
+					hsync_out => href_from_gauss, 
+					vsync_out => vsync_from_gauss, 
+					pixel_data_in => pixel_y_from_interface,  
+					pixel_data_out => pixel_from_gauss
+		);		
+		
+	
+	sobel0: sobel3x3
+		generic map(WIDTH => IMAGE_WIDTH,
+				  HEIGHT => IMAGE_HEIGHT)
+		port map(
+			clk => gls_clk ,
+			resetn => gls_resetn ,
+			pixel_clock => pxclk_from_gauss, hsync => href_from_gauss, vsync =>  vsync_from_gauss,
+			pixel_clock_out => pxclk_from_sobel, hsync_out => href_from_sobel, vsync_out => vsync_from_sobel, 
+			pixel_data_in => pixel_from_gauss,  
+			pixel_data_out => pixel_from_sobel
+		);	
+
+
+	harris_detector : HARRIS 
+	generic map(WIDTH => IMAGE_WIDTH, HEIGHT => IMAGE_HEIGHT, WINDOW_SIZE => 5, DS_FACTOR => 2)
+	port map(
+			clk => gls_clk,
+			resetn => gls_resetn, 
+			pixel_clock => pxclk_from_interface, 
+			hsync => href_from_interface,
+			vsync => vsync_from_interface, 
+			pixel_clock_out =>pxclk_from_harris,
+			hsync_out => href_from_harris, 
+			vsync_out => vsync_from_harris,
+			pixel_data_in =>  pixel_y_from_interface,
+			harris_out => harris_resp 
+	);
+	signed_harris_resp <= signed(harris_resp) ;
+
+	pixel_from_harris <=  (others => '0') when harris_resp(15) = '1' else
+								  harris_resp(7 downto 0) when signed_harris_resp < 256 else
+								  (others => '1');
+								  
+								  
+								  
+
+
+
+		video_switch_inst: video_switch
+		generic map(NB	=>  4)
+		port map(
+			pixel_clock(0) => pxclk_from_interface, 
+			--pixel_clock(0) => pxclk_from_classifier, 
+			pixel_clock(1) => pxclk_from_gauss, 
+			pixel_clock(2) => pxclk_from_sobel, 
+			pixel_clock(3) => pxclk_from_harris,
+
+			hsync(0) => href_from_interface,
+			--hsync(0) => href_from_classifier,
+			hsync(1) => href_from_gauss, 
+			hsync(2) => href_from_sobel,
+			hsync(3) => href_from_harris,
+
+
+			vsync(0) => vsync_from_interface,
+			--vsync(0) => vsync_from_classifier,
+			vsync(1) => vsync_from_gauss,
+			vsync(2) => vsync_from_sobel,
+			vsync(3) => vsync_from_harris,
+
+			pixel_data(0) => pixel_y_from_interface	,
+			--pixel_data(0) => pixel_from_classifier	,
+			pixel_data(1) => pixel_from_gauss	,
+			pixel_data(2) => pixel_from_sobel	,
+			pixel_data(3) => pixel_from_harris	,
+
+
+			pixel_clock_out => pxclk_from_switch, 
+			hsync_out => href_from_switch, 
+			vsync_out => vsync_from_switch,
+			pixel_data_out => pixel_from_switch,
+			channel(1 downto 0) => switch_value(1 downto 0),
+			channel(7 downto 2) => "000000"
 		);
+		switch_value <= SW ;
+	
 
-
-pix_to_fifo : pixel2fifo 
-generic map(ADD_SYNC => true)
-port map(
-	clk => gls_clk, resetn => sys_resetn,
---	sreset => pipeline_reset ,
-	pixel_clock => pxclk_from_interface, hsync => href_from_interface, vsync => vsync_from_interface,
-	pixel_data_in => pixel_y_from_interface,
-	--pixel_y => pixel_y_from_interface,
-	--pixel_u => pixel_u_from_interface,
-	--pixel_v => pixel_v_from_interface,
-	fifo_data => fifo_input, 
-	fifo_wr => fifoB_wr 
-);
+	pixel_to_fifo : yuv_pixel2fifo
+		port map(
+			clk => gls_clk, resetn => gls_resetn,
+			sreset => reset_pipeline , -- should wire to a register to allow to reset 
+			pixel_clock => pxclk_from_switch, 
+			hsync => href_from_switch, 
+			vsync => vsync_from_switch,
+			pixel_y => pixel_from_switch,
+			pixel_u => X"80",
+			pixel_v => X"80",
+			fifo_data => preview_fifo_input,
+			fifo_wr => preview_fifo_wr
+		);	
 
 
 
