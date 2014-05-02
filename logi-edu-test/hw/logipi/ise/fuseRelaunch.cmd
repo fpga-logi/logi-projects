57ae01db882db1792541d488b87d8d5624f1c175
Michael Jones <mjones@valentfx.com>
2014-05-02 12:01:38 -0600

    remove relauch.cmd

--- logi-edu-test/hw/logipi/ise/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/jpiat/development/FPGA/logi-family/logi-projects/logi-edu-test/logipi-hw/sseg_tb_isim_beh.exe" -prj "/home/jpiat/development/FPGA/logi-family/logi-projects/logi-edu-test/logipi-hw/sseg_tb_beh.prj" "work.sseg_tb" 
