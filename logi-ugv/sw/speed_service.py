0a0d5ba2ed2f4be5a7876a19ba3bc4c38fa73088
Jonathan Piat <piat.jonathan@gmail.com>
2014-06-15 19:31:33 +0200

    new service to get speed of car

---
+++ logi-ugv/sw/speed_service.py
@@ -0,0 +1,54 @@
+import math
+import logi
+import time	
+
+
+DISTANCE_BETWEEN_TICKS = 0.0048
+ENCODER_PERIOD_ADDRESS = 0x000E
+class SpeedService():
+		
+
+	def __init__(self):
+		self.current_speed = 0.0 ; 	
+
+	def getSpeed(self):
+		enc_reg = logi.logiRead(ENCODER_PERIOD_ADDRESS, 2)
+        	enc_val = (enc_reg[1] << 8) | enc_reg[0]
+        	if enc_val == 32767:
+                	self.current_speed = 0.0
+		elif enc_val == 0:
+			self.current_speed = 15.0
+        	self.current_speed = DISTANCE_BETWEEN_TICKS/(float(enc_val)/1000000.0)
+		return self.current_speed
+
+
+if __name__ == "__main__":
+	speed_service = SpeedService()
+	logi.logiWrite(0x0000, (0x01, 0x01)) # enable watchdog
+
+	logi.logiWrite(0x0013, (128, 0x00)) # set failsafe value for speed
+	logi.logiWrite(0x0012, (128, 0x00)) # set current value for speed
+	P = 30.0
+	I = 0.75
+	D = -2.0
+
+	target_speed = 4.0
+	old_error = 0.0
+	integral = 0.0
+	while True:
+        	logi.logiWrite(0x0000, (0x01, 0x01))
+        	speed = speed_service.getSpeed()
+        	error = (target_speed - speed)
+        	derivative = error - old_error
+        	old_error = error
+        	cmd = error * P + derivative * D + integral * I
+        	#print "speed : "+str(speed)+"m/s , error :"+str(error)+", deriv :"+str(derivative)+", integral :"+str(integral)
+        	integral = integral + error
+		if cmd < 0.0 :
+                	cmd = 0
+        	if cmd > 127.0:
+                	cmd = 127
+        	logi.logiWrite(0x0012, (int(cmd)+128, 0x00))
+		time.sleep(0.02)
+	
+	
