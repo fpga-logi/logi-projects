55ec084e86c1ebdd8ac24ab3ad959339ee17c758
Jonathan Piat <piat.jonathan@gmail.com>
2014-08-22 16:41:31 +0200

    now tests reads and write with configurable block size

--- logi-wishbone/sw/logibone/test_wishbone.c
+++ logi-wishbone/sw/logibone/test_wishbone.c
@@ -10,15 +10,18 @@
 //#include <linux/ioctl.h>
 #include <sys/ioctl.h>
 
+
+#define BLOCK_SIZE 65536
+
 int main(int argc, char ** argv){
 	int fd, address ;
 	unsigned short i ;
-	unsigned short writeVals [2048] ;
+	unsigned short writeVals [BLOCK_SIZE] ;
 	struct timeval temp1,temp2;
 	long elapsed_u_sec,elapsed_s_sec,elapsed_m_time,elapsed_u_time;
 	fd = open("/dev/logibone_mem", O_RDWR | O_SYNC);
 	gettimeofday(&temp1,NULL);
-	if(pwrite(fd, writeVals, 4096, 0) < 4096){
+	if(pwrite(fd, writeVals, BLOCK_SIZE*2, 0) < BLOCK_SIZE*2){
 		printf("Write error !");
 	}
 	gettimeofday(&temp2,NULL);
@@ -26,7 +29,20 @@ int main(int argc, char ** argv){
 	elapsed_u_sec=temp2.tv_usec-temp1.tv_usec;
 	elapsed_u_time=(elapsed_s_sec)*100000+elapsed_u_sec;	
 	printf("Time in Microsecond=%ld \n",elapsed_u_time);
-	printf("W Speed=====%d KB/Sec \n",(4096*1000)/elapsed_u_time );
+	printf("W Speed=====%d KB/Sec \n",(BLOCK_SIZE*2*1000)/elapsed_u_time );
+	
+	gettimeofday(&temp1,NULL);
+        if(pread(fd, writeVals, BLOCK_SIZE*2, 0) < BLOCK_SIZE*2){
+                printf("Write error !");
+        }
+        gettimeofday(&temp2,NULL);
+        elapsed_s_sec=temp2.tv_sec-temp1.tv_sec;
+        elapsed_u_sec=temp2.tv_usec-temp1.tv_usec;
+        elapsed_u_time=(elapsed_s_sec)*100000+elapsed_u_sec;    
+        printf("Time in Microsecond=%ld \n",elapsed_u_time);
+        printf("R Speed=====%d KB/Sec \n",(BLOCK_SIZE*2*1000)/elapsed_u_time );
+
+
 	close(fd);
 	return 0 ;
 }
