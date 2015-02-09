0665ff4c5caf952ff0f2ea6a05fe74805a53a0b0
Jonathan Piat <piat.jonathan@gmail.com>
2015-02-09 09:09:43 +0100

    removed reference to ip_core generated clock

--- logi-camera-demo/hw/logipi/hdl/logipi_camera.vhd
+++ logi-camera-demo/hw/logipi/hdl/logipi_camera.vhd
@@ -81,6 +81,7 @@ architecture Behavioral of logipi_camera is
 	
 	-- Systemc clocking and reset
 	signal clk_sys, clk_100,  clk_96, clk_24, clk_locked : std_logic ;
+	signal clk_100_unbuf,  clk_24_unbuf, osc_buff, clkfb : std_logic ;
 	signal resetn , sys_resetn : std_logic ;
 	
 	
@@ -145,16 +146,6 @@ begin
 	CS_FLASH <= '1' ;
 	
 	resetn <= PB(0) ;
-	
-	sys_clocks_gen: clock_gen 
-	PORT MAP(
-		CLK_IN1 => OSC_FPGA,
-		CLK_OUT1 => clk_100,
-		CLK_OUT2 => clk_24,
-		CLK_OUT3 => clk_96, --96Mhz system clock
-		LOCKED => clk_locked
-	);
-	clk_sys <= clk_96 ;
 
 	reset0: reset_generator 
 	generic map(HOLD_0 => 1000)
@@ -429,6 +420,48 @@ begin
 			fifo_wr => preview_fifo_wr,
 			sreset => '0'			--mj added, not sure if this will conflict.  
 		);	
+		
+		
+PLL_BASE_inst : PLL_BASE generic map (
+      BANDWIDTH      => "OPTIMIZED",        -- "HIGH", "LOW" or "OPTIMIZED" 
+      CLKFBOUT_MULT  => 12 ,                 -- Multiply value for all CLKOUT clock outputs (1-64)
+      CLKFBOUT_PHASE => 0.0,                -- Phase offset in degrees of the clock feedback output (0.0-360.0).
+      CLKIN_PERIOD   => 20.00,              -- Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
+      -- CLKOUT0_DIVIDE - CLKOUT5_DIVIDE: Divide amount for CLKOUT# clock output (1-128)
+      CLKOUT0_DIVIDE => 6,       CLKOUT1_DIVIDE => 25,
+      CLKOUT2_DIVIDE => 1,       CLKOUT3_DIVIDE => 1,
+      CLKOUT4_DIVIDE => 1,       CLKOUT5_DIVIDE => 1,
+      -- CLKOUT0_DUTY_CYCLE - CLKOUT5_DUTY_CYCLE: Duty cycle for CLKOUT# clock output (0.01-0.99).
+      CLKOUT0_DUTY_CYCLE => 0.5, CLKOUT1_DUTY_CYCLE => 0.5,
+      CLKOUT2_DUTY_CYCLE => 0.5, CLKOUT3_DUTY_CYCLE => 0.5,
+      CLKOUT4_DUTY_CYCLE => 0.5, CLKOUT5_DUTY_CYCLE => 0.5,
+      -- CLKOUT0_PHASE - CLKOUT5_PHASE: Output phase relationship for CLKOUT# clock output (-360.0-360.0).
+      CLKOUT0_PHASE => 0.0,      CLKOUT1_PHASE => 0.0, -- Capture clock
+      CLKOUT2_PHASE => 0.0,      CLKOUT3_PHASE => 0.0,
+      CLKOUT4_PHASE => 0.0,      CLKOUT5_PHASE => 0.0,
+      
+      CLK_FEEDBACK => "CLKFBOUT",           -- Clock source to drive CLKFBIN ("CLKFBOUT" or "CLKOUT0")
+      COMPENSATION => "SYSTEM_SYNCHRONOUS", -- "SYSTEM_SYNCHRONOUS", "SOURCE_SYNCHRONOUS", "EXTERNAL" 
+      DIVCLK_DIVIDE => 1,                   -- Division value for all output clocks (1-52)
+      REF_JITTER => 0.1,                    -- Reference Clock Jitter in UI (0.000-0.999).
+      RESET_ON_LOSS_OF_LOCK => FALSE        -- Must be set to FALSE
+   ) port map (
+      CLKFBOUT => clkfb, -- 1-bit output: PLL_BASE feedback output
+      -- CLKOUT0 - CLKOUT5: 1-bit (each) output: Clock outputs
+      CLKOUT0 => clk_100_unbuf,      CLKOUT1 => clk_24_unbuf,
+      CLKOUT2 => open,      CLKOUT3 => open,
+      CLKOUT4 => open,      CLKOUT5 => open,
+      LOCKED  => clk_locked,  -- 1-bit output: PLL_BASE lock status output
+      CLKFBIN => clkfb, -- 1-bit input: Feedback clock input
+      CLKIN   => osc_buff,  -- 1-bit input: Clock input
+      RST     => '0'    -- 1-bit input: Reset input
+   );
+
+    -- Buffering of clocks
+	BUFG_1 : BUFG port map (O => osc_buff,    I => OSC_FPGA);	
+	BUFG_2 : BUFG port map (O => clk_100,    I => clk_100_unbuf);
+	BUFG_3 : BUFG port map (O => clk_24,    I => clk_24_unbuf);
+
 
 end Behavioral;
 
